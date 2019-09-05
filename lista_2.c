6ebaf1817b93ad3c8c8731f37753a61875d6eede
lucasbezerra26 <lucasbezerra527@gmail.com>
2019-09-05 18:50:20 -0300

    Fazendo que se pede da questao anterior

--- lista_2.c
+++ lista_2.c
@@ -1,19 +1,20 @@
 #include<stdio.h> 
-#include<stdlib.h> 
+#include<stdlib.h>
+#include <time.h>
+
 
 struct arvAVL{ 
 	int info; 
 	struct arvAVL *esq; 
 	struct arvAVL *dir; 
 	int altura; 
-}; 
+};
 
 typedef struct arvAVL arvAVL;
 
 // Função utilizada para pegar o valor máximo entre os dois inteiros 
 int max(int a, int b); 
-
-
+arvAVL *busca(arvAVL **r, int k);
 void imprimir(arvAVL **raiz);
 
 
@@ -23,6 +24,10 @@ int altura(arvAVL *raiz){
 		return 0; 
 	return raiz->altura; 
 } 
+
+int gerarNumAleatorio(){
+    return ((rand() % 1000) + 1);
+}
 
 int max(int a, int b) { 
 	return (a > b)? a : b; 
@@ -57,17 +62,60 @@ void rotacaoLL(arvAVL **no) {
   *no = aux;
 }
 
-// rotação LR 
-void rotacaoLR(arvAVL **no) { 
-	rotacaoRR(&(*no));
-	rotacaoLL(&(*no));
+int numeroNos(arvAVL *raiz){
+	if(raiz == NULL ) return 0;
+	return numeroNos(raiz->esq)+1+numeroNos(raiz->dir);
 }
 
-// rotação LR 
-void rotacaoRL(arvAVL **no) { 
-	rotacaoLL(&(*no));
-	rotacaoRR(&(*no));
+void profundidade_maior(arvAVL **raiz,arvAVL **maior, int *nivel){
+    int numeroNoDir = numeroNos((*raiz)->dir);
+    int numeroNoEsq = numeroNos((*raiz)->esq);
+    if(!((*raiz)->dir == NULL && (*raiz)->esq == NULL)){
+        (*nivel)++;
+        if(numeroNoDir > numeroNoEsq){
+            (*maior) =  (*raiz)->dir;
+            profundidade_maior(&(*raiz)->dir, &(*maior), nivel);
+        }else{
+            (*maior) =  (*raiz)->esq;
+            profundidade_maior(&(*raiz)->esq, &(*maior), nivel);
+        }
+    }
 }
+
+void profundidade_menor(arvAVL **raiz,arvAVL **menor, int *nivel){
+    int numeroNoDir = numeroNos((*raiz)->dir);
+    int numeroNoEsq = numeroNos((*raiz)->esq);
+    if( ((*raiz)->dir != NULL) && ((*raiz)->esq != NULL) ){
+        (*nivel)++;
+        if(numeroNoDir < numeroNoEsq){
+            (*menor) =  (*raiz)->dir;
+            profundidade_menor(&(*raiz)->dir, &(*menor), nivel);
+        }else{
+            (*menor) =  (*raiz)->esq;
+            profundidade_menor(&(*raiz)->esq, &(*menor), nivel);
+        }
+    }else{
+        if ((*raiz)->dir == NULL && (*raiz)->esq != NULL){
+            profundidade_menor(&(*raiz)->esq, &(*menor), nivel);
+        }else{
+            if( (*raiz)->dir != NULL && (*raiz)->esq == NULL ){
+                profundidade_menor(&(*raiz)->dir, &(*menor), nivel);
+            }else{
+                (*menor) = (*raiz);
+            }
+        }
+    }
+}
+
+arvAVL *busca (arvAVL **r, int k) {
+    if (*r == NULL || (*r)->info == k)
+       return *r;
+    if ((*r)->info > k)
+       return busca(&(*r)->esq, k);
+    else
+       return busca(&(*r)->dir, k);
+}
+
 
 // fator de balanceamento 
 int fatorBalanceamento(arvAVL *raiz){ 
@@ -123,18 +171,71 @@ void imprimir(arvAVL **raiz){
   }
 }
 
-int main(){ 
-  arvAVL *root = NULL; 
+int main(){
+    arvAVL *raiz, *aux;
+    arvAVL *maior;
+    int nivel_menor = 0, nivel_maior;
+    srand(time(NULL));
+    int *vetor;
+    vetor = (int*) calloc(sizeof(int),1000);
+    for (int i = 0; i < 30; i++){
+        nivel_menor = 0;
+        nivel_maior =  0;
 
-  inserir(&root, 10); 
-  inserir(&root, 20); 
-  inserir(&root, 30); 
-  inserir(&root, 40); 
-  inserir(&root, 50); 
-  inserir(&root, 25); 
+        printf("+----------------------------+\n");
+        printf("======= %d INTERAÇÃO =========\n", i+1);
+        printf("+----------------------------+\n");
+        
+        raiz = NULL;
+        // maior = NULL;
+        clock_t inicio = clock();
 
-  imprimir(&root);
-  printf("\n");
+        for(int i = 0; i < 5; i++){
+            int num = gerarNumAleatorio();
+            inserir(&raiz, num);
+        }
+        clock_t fim = clock();
+        float segundos = (float)(fim - inicio) / CLOCKS_PER_SEC;
+        printf("Inserido 1000 elementos em %.4f segundos.\n", segundos);
+        imprimir(&raiz);
+        printf("\n");
 
-  return 0; 
-} 
+        profundidade_maior(&raiz, &maior, &nivel_maior);
+        printf("Maior Nível: %d\n", nivel_maior);
+
+        profundidade_menor(&raiz, &aux, &nivel_menor);
+        printf("Menor Nível: %d\n", nivel_menor);
+        // printf("Num: %d \n", num);
+
+        inicio = clock();
+        busca(&raiz, 30);
+        fim = clock();
+        segundos = (float)(fim - inicio) / CLOCKS_PER_SEC;
+        printf("Busca do elemento 30. Demorou em %.4f segundos.\n", segundos);
+        int difrenca = nivel_maior-nivel_menor;
+        vetor[difrenca]++;
+    }
+    for(int i=0; i<1000; i++){
+        if( vetor[i] != 0 ){
+            printf("A quantidade de vezes que a diferenca foi %d é %d\n",i,vetor[i]);
+        }
+    }
+    return 0;
+}
+
+
+// int main(){ 
+//   arvAVL *root = NULL; 
+
+//   inserir(&root, 10); 
+//   inserir(&root, 20); 
+//   inserir(&root, 30); 
+//   inserir(&root, 40); 
+//   inserir(&root, 50); 
+//   inserir(&root, 25); 
+
+//   imprimir(&root);
+//   printf("\n");
+
+//   return 0; 
+// } 
