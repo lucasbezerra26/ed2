2322eb5dca0ef1a489f7c4bb8b0418cc47f3234c
lucasbezerra26 <lucasbezerra527@gmail.com>
2019-09-14 23:11:18 -0300

    Funcao de inserir e struct

--- lista_3.c
+++ lista_3.c
@@ -1,3 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
+struct arv{
+    char palavra[100];
+    char equivalentes[200];
+    char estrutura[100];
+    struct arv *dir,*esq;
+};
+typedef struct arv arv;
+
+
+void inserir(arv **raiz, arv **no){
+    if( *raiz == NULL){
+        *raiz = no;
+    }else{
+        if (strcmp((*no)->palavra, (*raiz)->palavra) == -1) //se menor
+        {
+            inserir(&(*raiz)->esq, no);
+        }else{
+            if (strcmp((*no)->palavra, (*raiz)->palavra) == 1)//se maior, se igual a 0 é pq é igual e não pode ter palavra igual
+                inserir(&(*raiz)->dir, no);
+        }
+    }
+}
+
+int main(){
+    return 0;
+}
